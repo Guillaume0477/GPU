906b57476ebf405b29ba4f389411339fe61a382a
Antoine Bralet <antoine.bralet@tpg10.cpe.fr>
2020-10-08 11:36:10 +0200

    fix pb

--- main.cpp
+++ main.cpp
@@ -144,12 +144,12 @@ void init()
   programme = creation_programme(vs,fs);
 
   // Créer un tableau de float contenant les sommets à afficher
-  float sommet_objet[] = {-1,-1,0, 1,-1,0 ,-1,1,0, 1,1,0};
+  float sommet_objet[] = {-1.0,-1.0,0.0, 1.0,-1.0,0.0 ,-1.0,1.0,0.0, 1.0,1.0,0.0};
   //sommet_objet.push_back([0,0,0, 1,0,0 ,0,1,0, 1,1,0]);
 
   // Créer un ficher d'entier non signé contenant les indices de sommets
   
-  uint indice_objet[] = {0,1,2,2,1,3};
+  uint indice_objet[] = {0,2,1,2,3,1};
 
   // Créer un VAO -> glGenVertexArrays(GLsizei, GLuint *)
   glGenVertexArrays(1, &VAO);
@@ -161,13 +161,13 @@ void init()
   glBindVertexArray(VAO);
 
   glBindBuffer(GL_ARRAY_BUFFER,VBO);
-  glBufferData(GL_ARRAY_BUFFER, 12, &sommet_objet[0], GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, 12*sizeof(float), &sommet_objet[0], GL_STATIC_DRAW);
   glVertexAttribPointer(0,3,GL_FLOAT, GL_FALSE, 0, 0);
   glEnableVertexAttribArray(0);
 
 
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6, &indice_objet[0], GL_STATIC_DRAW);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6*sizeof(uint), &indice_objet[0], GL_STATIC_DRAW);
 
  
 
