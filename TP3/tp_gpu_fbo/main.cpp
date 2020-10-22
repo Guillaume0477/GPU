8383517867b61c91b830774463100d790e8652e9
Ant89ne <antoine.bralet@cpe.fr>
2020-10-22 08:07:27 +0200

    TP3 FBO OK

--- TP3/tp_gpu_fbo/main.cpp
+++ TP3/tp_gpu_fbo/main.cpp
@@ -24,6 +24,11 @@ Camera cam;
 // Current program id
 GLuint program_id;
 
+// Frame buffer to use
+GLuint frame_buffer;
+GLuint tex_frame_buf;
+GLuint render_buffer;
+unsigned char* im_text_fb;
 
 void init()
 {
@@ -43,6 +48,28 @@ void init()
     Mesh m_wall = Mesh::create_grid(2);
     m_wall.apply_matrix(glm::translate(glm::mat4(1.), glm::vec3(0.f,0.35f,0.f)));
     VAO_wall = m_wall.load_to_gpu();
+
+    //Creation of a frame buffer object
+    glGenFramebuffers(1,&frame_buffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, frame_buffer);
+
+    glGenTextures(1, &tex_frame_buf);
+    glBindTexture(GL_TEXTURE_2D, tex_frame_buf);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, cam.width(), cam.height() , 0, GL_RGB, GL_UNSIGNED_BYTE, im_text_fb);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);    
+
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tex_frame_buf, 0);
+
+    glGenRenderbuffers(1, &render_buffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, render_buffer);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, cam.width(), cam.height());
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, render_buffer);
+
+    if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+      std::cout << "PB FRAME BUFFER" << std::endl;
 }
 
 void set_uniform_mvp(GLuint program)
@@ -62,17 +89,39 @@ static void display_callback()
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Affichage du sol
-    glUseProgram(program_id);
-    glBindVertexArray(VAO_wall);
-    set_uniform_mvp(program_id);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glBindTexture(GL_TEXTURE_2D, texture);
 
+    glBindFramebuffer(GL_FRAMEBUFFER, frame_buffer);
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     // Affichage de l'écureil
     glUseProgram(program_id);
     glBindVertexArray(VAO);
     set_uniform_mvp(program_id);
     glDrawElements(GL_TRIANGLES, n_elements, GL_UNSIGNED_INT, 0);
+
+    glhelper::print_screen(cam.width(), cam.height(), "firstAttempt.png");
+
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glBindTexture(GL_TEXTURE_2D, tex_frame_buf);
+    // // Affichage du sol
+    glUseProgram(program_id);
+    glBindVertexArray(VAO_wall);
+    set_uniform_mvp(program_id);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glUseProgram(program_id);
+    glBindVertexArray(VAO);
+    set_uniform_mvp(program_id);
+    glDrawElements(GL_TRIANGLES, n_elements, GL_UNSIGNED_INT, 0);
+
+    
+
+    
 
     glBindVertexArray(0);
     glutSwapBuffers ();
